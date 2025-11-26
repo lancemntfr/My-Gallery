9df9e0664a84d794b0540a4ab31139f376895e43
lancemntfr <mendozalance246@gmail.com>
2025-11-26 15:11:57 +0800

    Finished the initial layouts of the cards

---
+++ photos.js
@@ -0,0 +1,65 @@
+var photos = [
+    {
+       "image": "assets/images/6055609738394799205.jpg",
+       "id": "a",
+       "album": "",
+       "title": "Mee",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6055609738394799210.jpg",
+       "id": "2",
+       "album": "",
+       "title": "Mee2",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6055609738394799211.jpg",
+       "id": "3",
+       "album": "",
+       "title": "Mee3",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6125259728385412299.jpg",
+       "id": "4",
+       "album": "",
+       "title": "IDK",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6129708519935118357.jpg",
+       "id": "5",
+       "album": "",
+       "title": "Hiiii",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6129708519935118363.jpg",
+       "id": "5",
+       "album": "",
+       "title": "Woahhh",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6330252263951109517 (1).jpg",
+       "id": "6",
+       "album": "",
+       "title": "Sheshhh",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6330252263951109518.jpg",
+       "id": "7",
+       "album": "",
+       "title": "Lancccee",
+       "description": ""
+    },
+    {
+       "image": "assets/images/63302522639511095156.jpg",
+       "id": "8",
+       "album": "",
+       "title": "Yowww",
+       "description": ""
+    }
+];
