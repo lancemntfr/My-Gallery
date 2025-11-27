9aee8901ccebeb46dcf02220d73c5c64f697eedb
lancemntfr <mendozalance246@gmail.com>
2025-11-28 01:51:47 +0800

    Add a date in the gallery

--- photos.js
+++ photos.js
@@ -1,74 +1,121 @@
 var photos = [
     {
-       "image": "assets/images/6055609738394799205.jpg",
-       "id": "a",
-       "date": "11-21-2025",
+       "image": "assets/images/6125259728385412309.jpg",
+       "date": "Nov 21",
        "album": "",
-       "title": "Mee",
+       "title": "",
        "description": ""
     },
     {
-       "image": "assets/images/6055609738394799210.jpg",
-       "id": "b",
-       "date": "11-21-2025",
+       "image": "assets/images/6125259728385412315.jpg",
+       "date": "Nov 21",
        "album": "",
-       "title": "Mee2",
+       "title": "",
        "description": ""
     },
     {
-       "image": "assets/images/6055609738394799211.jpg",
-       "id": "c",
-       "date": "11-21-2025",
+       "image": "assets/images/6125259728385412316.jpg",
+       "date": "Nov 21",
        "album": "",
-       "title": "Mee3",
+       "title": "",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6127511528199097257.jpg",
+       "date": "Nov 21",
+       "album": "",
+       "title": "",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6127511528199097258.jpg",
+       "date": "Nov 21",
+       "album": "",
+       "title": "",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6127511528199097260.jpg",
+       "date": "Nov 21",
+       "album": "",
+       "title": "",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6129708519935118302.jpg",
+       "date": "Nov 21",
+       "album": "",
+       "title": "",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6129708519935118353.jpg",
+       "date": "Nov 21",
+       "album": "",
+       "title": "",
        "description": ""
     },
     {
        "image": "assets/images/6125259728385412299.jpg",
-       "id": "d",
-       "date": "",
+       "date": "Nov 21",
        "album": "",
        "title": "IDK",
        "description": ""
     },
     {
        "image": "assets/images/6129708519935118357.jpg",
-       "id": "e",
-       "date": "",
+       "date": "Nov 21",
        "album": "",
        "title": "Hiiii",
        "description": ""
     },
     {
        "image": "assets/images/6129708519935118363.jpg",
-       "id": "f",
-       "date": "",
+       "date": "Nov 21",
        "album": "",
        "title": "Woahhh",
        "description": ""
     },
     {
        "image": "assets/images/6330252263951109517 (1).jpg",
-       "id": "g",
-       "date": "",
+       "date": "Nov 12",
        "album": "",
        "title": "Sheshhh",
        "description": ""
     },
     {
        "image": "assets/images/6330252263951109518.jpg",
-       "id": "h",
-       "date": "",
+       "date": "Nov 12",
        "album": "",
        "title": "Lancccee",
        "description": ""
     },
     {
        "image": "assets/images/63302522639511095156.jpg",
-       "id": "i",
-       "date": "",
+       "date": "Nov 12",
        "album": "",
        "title": "Yowww",
        "description": ""
-    }
+    },
+    {
+       "image": "assets/images/6055609738394799205.jpg",
+       "date": "Oct 28",
+       "album": "",
+       "title": "Mee",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6055609738394799210.jpg",
+       "date": "Oct 28",
+       "album": "",
+       "title": "Mee2",
+       "description": ""
+    },
+    {
+       "image": "assets/images/6055609738394799211.jpg",
+       "date": "Oct 28",
+       "album": "",
+       "title": "Mee3",
+       "description": ""
+    },
 ];
